f1c0319fb6c60eedd37c6cb716ef0033536d290b
RooiGevaar19 <redthreat19@gmail.com>
2020-09-20 22:57:39 +0200

    all gradlew run

--- src/main/java/spong/SpongApplication.java
+++ src/main/java/spong/SpongApplication.java
@@ -3,11 +3,15 @@ package spong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.joda.time.LocalTime;
+
 @SpringBootApplication
 public class SpongApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Spong Server App");
+		LocalTime currentTime = new LocalTime();
+    		System.out.println("The current local time is: " + currentTime);
 		SpringApplication.run(SpongApplication.class, args);
 	}
 
