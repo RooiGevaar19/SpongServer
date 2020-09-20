acb55b5c6917acd235eecb941a36f694fa0bc206
RooiGevaar19 <redthreat19@gmail.com>
2020-09-20 22:34:34 +0200

    First commit

---
+++ src/main/java/com/example/spong/SpongApplication.java
@@ -0,0 +1,14 @@
+package com.example.spong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpongApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Spong Server App");
+		SpringApplication.run(SpongApplication.class, args);
+	}
+
+}
